51269590e831e4c4f1666f380146191ac795f673
Carthur83 <christian_A_83@hotmail.com>
2025-06-06 07:37:49 +0200

    added key vault connection

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,3 +1,4 @@
+using Azure.Identity;
 using Microsoft.EntityFrameworkCore;
 using WebApi;
 using WebApi.Data.Contexts;
@@ -5,6 +6,15 @@ using WebApi.Data.Repositories;
 using WebApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var keyVaultUrl = builder.Configuration["AzureKeyVault:VaultUri"];
+
+if (!string.IsNullOrEmpty(keyVaultUrl))
+{
+    var credential = new DefaultAzureCredential();
+    builder.Configuration.AddAzureKeyVault(new Uri(keyVaultUrl), credential);
+}
+
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
@@ -13,7 +23,7 @@ builder.Services.AddGrpcClient<EventContract.EventContractClient>(x =>
     x.Address = new Uri("https://ca-eventservice-ggbpdjfxatc3cmch.swedencentral-01.azurewebsites.net");
 });
 
-builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("BookingsDb")));
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection")));
 builder.Services.AddScoped<IBookingRepository, BookingRepository>();
 builder.Services.AddScoped<IBookingService, BookingService>();
 
